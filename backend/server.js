5dff1b2ded5aca3d42b8e0e965f0a32ae4cf9690
Veeraharikumar.S <vhkumar10@gmail.com>
2022-03-04 20:24:25 +0530

    First commit

---
+++ backend/server.js
@@ -0,0 +1,60 @@
+import express from "express";
+import mongoose from "mongoose";
+//import data from "./data.js";
+import userRouter from "./routers/userRouter.js";
+import productRouter from "./routers/productRouter.js";
+import orderRouter from "./routers/orderRouter.js";
+import dotenv from "dotenv";
+import path from "path";
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+mongoose.connect(process.env.MONGODB_URL || "mongodb://localhost/amazona");
+
+/*
+app.get("/api/products/:id", (req, res) => {
+  const product = data.products.find((x) => x._id === req.params.id);
+
+  if (product) {
+    res.send(product);
+  } else {
+    res.status(404).send({ message: "Product not Found" });
+  }
+});
+*/
+
+/*
+app.get("/api/products", (req, res) => {
+  res.send(data.products);
+});
+*/
+
+app.use("/api/users", userRouter);
+app.use("/api/products", productRouter);
+app.use("/api/orders", orderRouter);
+app.get("/api/config/paypal", (req, res) => {
+  res.send(process.env.PAYPAL_CLIENT_ID || "sb");
+});
+app.get("/", (req, res) => {
+  res.send("Server is ready !");
+});
+
+const __dirname = path.resolve();
+app.use(express.static(path.join(__dirname, "/frontend/build")));
+app.get("*", (req, res) =>
+  res.sendFile(path.join(__dirname, "/frontend/build/index.html"))
+);
+
+app.use((err, req, res, next) => {
+  res.status(500).send({ message: err.message });
+});
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log("Serve at http://localhost:" + port);
+});
