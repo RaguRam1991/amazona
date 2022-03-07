4d4b26fd6fd74318f1ad3734f949750ba5f44fa8
raguram1991 <6262ragu@gmail.com>
2022-03-07 18:29:26 +0530

    removed initial get method app.get()

--- backend/server.js
+++ backend/server.js
@@ -55,9 +55,12 @@ app.use("/api/orders", orderRouter);
 app.get("/api/config/paypal", (req, res) => {
   res.send(process.env.PAYPAL_CLIENT_ID || "sb");
 });
+
+/*
 app.get("/", (req, res) => {
   res.send("Server is ready !");
 });
+*/
 
 const __dirname = path.resolve();
 app.use(express.static(path.join(__dirname, "/frontend/build")));
